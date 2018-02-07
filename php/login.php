24978b15ab77556a90d453019d25388efaf063a5
PatilRohitRP <rohit.patil.rrp16@gmail.com>
2018-02-07 19:41:29 +0530

    First Commit

---
+++ php/login.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/login.css">
+<title>Login</title>
+</head>
+<body> 
+<div class="header">
+ <p>Digital Kisaan<p>
+ <ul>
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+ </div>
+ </div>
+ <div class="login">
+     <p align="center">Login </p>
+     <input type="text" placeholder="Enter username"><br><br>
+	 <input type="password" placeholder="Enter password"><br><br>
+     <input  type="submit" value="Login">
+   <div class="footr">
+     <p>New user ? Click <a href="./Signup.php">here</a> to create an account</p>    
+   </div>	 
+ </div>
+</html>
