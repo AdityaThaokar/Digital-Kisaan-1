eb9a19813d4c3bb9e31741f2a36e25cfb8b45690
PatilRohitRP <rohit.patil.rrp16@gmail.com>
2018-02-18 04:18:15 +0530

    yassssssssssssssssssssssssssssssss
    workingggggggggggggggggg

--- php/maps.php
+++ php/maps.php
@@ -27,18 +27,16 @@ $connect = mysqli_connect("localhost", "root", "", "digital_kisan");
 	while ($row = mysqli_fetch_array($sel)) {
     $storeArray[] =  $row['location'];  
 	}
-	print_r($storeArray);
 	echo '<div class="card-block" align="center">Your Location: '.$_SESSION['location'].'</div>';
   	?>
     <div id="map" class="card-block"></div>
      <div class="card-block">
           <input type="button" value="Your Location" onclick="codeAddress()" class="btn btn-success">
-          <input type="button" value="Show nearby shops" onclick="shopmarkers()" class="btn btn-success">
+          <input type="button" value="Show nearby shops" onclick="showshop()" class="btn btn-success">
         </div>
     <script>
       	var map;
 	var geocoder;		
-
 	function initMap() {
 		var mapLayer = document.getElementById("map");
 		var centerCoordinates = new google.maps.LatLng(37.6, -95.665);
@@ -47,28 +45,45 @@ $connect = mysqli_connect("localhost", "root", "", "digital_kisan");
 		map = new google.maps.Map(mapLayer, defaultOptions);
 		geocoder = new google.maps.Geocoder();
 		
-		<?php 
-            if(!empty($countryResult)) 
-            {
-                foreach($countryResult as $k=>$v)
-                {   
-         ?>  
-         	geocoder.geocode( { 'address': '<?php echo $countryResult[$k]["country"]; ?>' }, function(LocationResult, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					var latitude = LocationResult[0].geometry.location.lat();
-					var longitude = LocationResult[0].geometry.location.lng();
-				} 
-        	    		new google.maps.Marker({
-            	        position: new google.maps.LatLng(latitude, longitude),
-            	        map: map,
-            	        title: '<?php echo $countryResult[$k]["country"]; ?>'
-            	    });
-			});
-	    <?php
-                }
-            }
-	    ?>		
-	}
+}
+
+function showshop(){
+	<?php
+	foreach($storeArray as $keys => $values)  
+	{ ?>
+		var address = '<?php echo $values; ?>';
+		  geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            animation: google.maps.Animation.BOUNCE 
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+<?php } ?>
+}
+
+function codeAddress(){
+	 var address = '<?php echo $_SESSION['location']; ?>';
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            animation: google.maps.Animation.BOUNCE,
+            title: "<?php echo $_SESSION['username']; ?>'s Location"
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+
+}	
 	</script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMNq3nuw5whsc1fdAbwX4bHWMLgCt4C6E&callback=initMap"
     async defer></script>
